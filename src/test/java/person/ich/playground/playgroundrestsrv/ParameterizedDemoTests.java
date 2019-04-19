ad4729e2b806f1ea97752151c4581bb757b38b74
Martin <martin.takats@t-online.de>
2019-04-19 22:58:07 +0200

    Parameterized demo test

--- src/test/java/person/ich/playground/playgroundrestsrv/ParameterizedDemoTests.java
+++ src/test/java/person/ich/playground/playgroundrestsrv/ParameterizedDemoTests.java
@@ -3,13 +3,15 @@ package person.ich.playground.playgroundrestsrv;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.Arrays;
 import java.util.Collection;
 
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertEquals;
 
 @RunWith(Parameterized.class)
 @SpringBootTest
@@ -17,7 +19,7 @@ public class ParameterizedDemoTests {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Parameterized.Parameters
+    @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
                 {"param1", "param1_ext"},
@@ -38,7 +40,7 @@ public class ParameterizedDemoTests {
     public void parameterizedTest() {
         String result = input + "_ext";
         log.info("++++++++++ parameterized test: result: " + result + " expected: " + expected);
-        assertTrue(result.equals(expected));
+        assertEquals(result, expected);
     }
 
 }
