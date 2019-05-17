c70722eff71d900bc6cf902638277804dd581aa2
Martin <martin.takats@t-online.de>
2019-05-17 21:41:26 +0200

    Autowired dataprovider tests

--- src/test/java/person/ich/playground/playgroundrestsrv/DemoUtilDataproviderTest.java
+++ src/test/java/person/ich/playground/playgroundrestsrv/DemoUtilDataproviderTest.java
@@ -1,9 +1,11 @@
 package person.ich.playground.playgroundrestsrv;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,36 +13,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContextManager;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import static junit.framework.TestCase.assertTrue;
 
-@RunWith(Parameterized.class)
+@RunWith(DataProviderRunner.class)
 @SpringBootTest
 @ContextConfiguration
-public class DemoUtilParameterizedTest {
+public class DemoUtilDataproviderTest {
 
     private static final TestContextManager testContextManager = new TestContextManager(DemoUtilParameterizedTest.class);
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private int min;
-    private int max;
 
     @Autowired
     private DemoUtil demoUtil;
 
-    public DemoUtilParameterizedTest(int min, int max) {
-        this.min = min;
-        this.max = max;
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
+    @DataProvider
+    public static Object[][] dataProvider() {
+        return new Object[][]{
                 {-10, 0},
                 {0, 10},
                 {-10, 10}
-        });
+        };
     }
 
     @Before
@@ -49,9 +41,11 @@ public class DemoUtilParameterizedTest {
     }
 
     @Test
-    public void getRandomIntegerTest() {
+    @UseDataProvider("dataProvider")
+    public void getRandomIntegerTest(int min, int max) {
         int random = demoUtil.getRandomInteger(min, max);
         log.info("++++++++++ getRandomIntegerTest: random = " + random);
         assertTrue((random >= min) && random <= max);
     }
+
 }
