fc3f4138c528da21a40e23459a900f520288c015
Martin Takats <martin.takats@hotmail.de>
2019-10-29 21:02:26 +0100

    some more swagger docu

--- src/main/java/person/ich/playground/playgroundrestsrv/DemoController.java
+++ src/main/java/person/ich/playground/playgroundrestsrv/DemoController.java
@@ -1,6 +1,8 @@
 package person.ich.playground.playgroundrestsrv;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
-@Api(tags = "Demo controller", value = "DemoQueries", description = "Supported queries")
+@Api(tags = "Demo controller", value = "DemoQueries", description = "Supported requests")
 public class DemoController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -17,18 +19,25 @@ public class DemoController {
     private final static String s2 = "Hello %s! ";
 
     @GetMapping(value = {"/ping"}, produces = MediaType.TEXT_PLAIN_VALUE)
-    public String version() {
+    @ApiOperation(value = "Ping me", notes = "Some notes to ping")
+    public String ping() {
         return "pong";
     }
 
     @GetMapping(value = {"/hello"}, produces = MediaType.TEXT_PLAIN_VALUE)
-    public String hello(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
+    @ApiOperation(value = "Say hello with name in query parameter", notes = "Some notes to hello with name in query parameter \"?name={name}\"")
+    public String hello(
+            @ApiParam(name = "name", value = "name", defaultValue = "Query Ballo")
+            @RequestParam(value = "name", required = false, defaultValue = " Query Ballo") String name) {
         log.info("/hello called with: " + name);
         return name.equals("") ? s1 : (String.format(s2, name) + s1);
     }
 
     @GetMapping(value = {"/hello/{name}"}, produces = MediaType.TEXT_PLAIN_VALUE)
-    public String helloPathVariable(@PathVariable String name) {
+    @ApiOperation(value = "Say hello with name in path", notes = "Some notes to hello with name in path")
+    public String helloPathVariable(
+            @ApiParam(name = "name", value = "name", defaultValue = "Path Ballo")
+            @PathVariable String name) {
         log.info("/hello/{name} called with: " + name);
         return String.format(s2, name) + s1;
     }
--- src/main/java/person/ich/playground/playgroundrestsrv/DocumentationConfig.java
+++ src/main/java/person/ich/playground/playgroundrestsrv/DocumentationConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -18,6 +22,7 @@ public class DocumentationConfig {
     @Bean
     public Docket v1() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(endpoints())
@@ -25,5 +30,18 @@ public class DocumentationConfig {
     }
     private Predicate<String> endpoints() {
         return Predicates.or(PathSelectors.regex("/v1/.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Playground REST service API",
+                "This service is used as demo for some features",
+                "0.0.1",
+                "Term of service",
+                new Contact("Example Author", "http://www.example.com", "example.author@example.com"),
+                "Example license",
+                "http://example.com",
+                Collections.EMPTY_LIST
+        );
     }
 }
